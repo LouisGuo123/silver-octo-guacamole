de63b5486aa7aa49af1411aa6cda1461de2487c8
Louis Guo <72518454+LouisGuo123@users.noreply.github.com>
2023-07-01 13:17:14 -0400

    Update some things

---
+++ sandbox-page/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  document.getElementById("html-body").style.overflow = "auto";
+  document.getElementById("html-body").style.height = "initial";
+  document.getElementById("load-cover").style.display = "none";
+};
