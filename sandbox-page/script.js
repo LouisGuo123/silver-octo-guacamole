99f43760c5fc7d3e1848e139584008d14b734226
Louis Guo <72518454+LouisGuo123@users.noreply.github.com>
2023-07-01 15:03:03 -0400

    Update script.js

--- sandbox-page/script.js
+++ sandbox-page/script.js
@@ -3,3 +3,7 @@ window.onload = function () {
   document.getElementById("html-body").style.height = "initial";
   document.getElementById("load-cover").style.display = "none";
 };
+
+window.onscroll = function (event) {
+  console.log(window.scrollY);
+};
