fed606d304ca08eff98c5b85a057c28a173e9346
Louis Guo <72518454+LouisGuo123@users.noreply.github.com>
2023-07-03 14:58:54 -0400

    Update everything

--- sandbox-page/script.js
+++ sandbox-page/script.js
@@ -1,3 +1,16 @@
 function openInNewTab(url) {
   window.open(url, "_blank").focus();
 }
+
+function chooseRandomChild(element) {
+  var len = element.children.length;
+  var index = Math.floor(Math.random() * len);
+  for (child of element.children) {
+    child.style.display = "none";
+  }
+  element.children[index].style.display = "initial";
+}
+
+document.querySelectorAll(".choose-random-child").forEach((value, i, obj) => {
+  chooseRandomChild(value);
+});
